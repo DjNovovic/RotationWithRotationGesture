b34e88095b50a19e723b78fc7ca92bf96d61d277
DjNovovic <djordjijenovovic@gmail.com>
2021-08-29 09:23:54 +0200

    Initial Commit

---
+++ RotationWithRotationGesture/RotationWithRotationGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotationWithRotationGestureApp.swift
+//  RotationWithRotationGesture
+//
+//  Created by Đorđije Novović on 29.8.21..
+//
+
+import SwiftUI
+
+@main
+struct RotationWithRotationGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
