df7cdac5aa79be05294ea3a4d7919ef0fa0ac25c
DjNovovic <djordjijenovovic@gmail.com>
2021-08-29 09:27:50 +0200

    RotationGestureApp

--- RotationWithRotationGesture/ContentView.swift
+++ RotationWithRotationGesture/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var angle: Angle = Angle(degrees: 0)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Image(systemName: "heart.circle.fill")
+            .font(.largeTitle)
+            .foregroundColor(.orange)
+            .scaleEffect(4)
+            .padding(64)
+            .background(Color.secondary.cornerRadius(16).opacity(0.8))
+            .rotationEffect(angle)
+            .gesture(
+                RotationGesture()
+                    .onChanged { value in
+                        angle = value
+                    }
+                    .onEnded { value in
+                        withAnimation(.spring()) {
+                            angle = Angle(degrees: 0)
+                        }
+                    }
+            )
     }
 }
 
